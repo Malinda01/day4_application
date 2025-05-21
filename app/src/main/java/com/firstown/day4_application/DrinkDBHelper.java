037eb5cb6aea16ef37caf5babfe9a13a4c8976b9
Malinda Amarakoon <greshanamarakoon@gmail.com>
2025-05-21 14:07:07 +0530

    Insert and Update data Version 01

--- app/src/main/java/com/firstown/day4_application/DrinkDBHelper.java
+++ app/src/main/java/com/firstown/day4_application/DrinkDBHelper.java
@@ -1,12 +1,13 @@
 package com.firstown.day4_application;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DrinkDBHelper extends SQLiteOpenHelper {
 
-    private static String dbName = "Drinks Application";
+    private static String dbName = "day4_application";
     private static int dbVersion = 1;
 
     public DrinkDBHelper(Context context){
@@ -16,11 +17,43 @@ public class DrinkDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE Drinks (" +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "DrinksName TEXT, " +
+                "description TEXT)";
 
+        db.execSQL(sql);
+
+        this.addSomeData(db, "Kolakanda", "Energy Drink");
+        this.addSomeData(db, "Koththamalli", "Good for health");
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+
+    }
+
+    // Insert data into the database
+    public void addSomeData(SQLiteDatabase db, String DrinksName, String description) {
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("DrinksName", DrinksName);
+        contentValues.put("description", description);
+
+        db.insert("Drinks", null, contentValues);
+
+    }
+
+    // Update Description in the database
+    public void updateData(SQLiteDatabase db, int id, String DrinksName, String description) {
+        ContentValues contentValues = new ContentValues();
+
+//        contentValues.put("DrinksName", DrinksName);
+        contentValues.put("description", description);
+
+        db.update("Drinks", contentValues, "DrinksName = ?", new String[]{"Pepsi"});
 
     }
 
